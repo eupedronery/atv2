56c24979518a95a802dbda9e883bde4739e149a1
Pedro Nery <100791953+eupedronery@users.noreply.github.com>
2022-06-02 11:10:51 -0300

    Update princi.java

--- princi.java
+++ princi.java
@@ -1,5 +1,25 @@
 package view;
 
-public class princi {
+import java.io.IOException;
+import java.util.List;
+import java.util.Stack;
 
+import controller.FifaController;
+
+public class princi {
+	  public static void main(String [] args) {
+		String caminho = "C:\\TEMP";
+		String nome = "data.csv";
+		FifaController fifa = new FifaController();
+		try {
+			Stack <String> pilha = fifa.empilhaBrasileiros(caminho, nome);
+			List<String> lista = fifa.listaRevelacoes(caminho, nome);
+			fifa.desempilhaBonsBrasileiros(pilha);
+			fifa.buscaListaBonsJovens(lista);
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+	}
 }
